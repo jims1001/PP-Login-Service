5d7ec85c0e885378b5fb7897bc4a4d1b6e1a8aa4
jed <166913797+jims1001@users.noreply.github.com>
2025-12-12 16:31:21 +0400

    创建工程

---
+++ src/main/java/com/PPCloud/PP_Login_Service/PpLoginServiceApplication.java
@@ -0,0 +1,13 @@
+package com.PPCloud.PP_Login_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpLoginServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpLoginServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PPCloud/PP_Login_Service/PpLoginServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PPCloud.PP_Login_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpLoginServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
